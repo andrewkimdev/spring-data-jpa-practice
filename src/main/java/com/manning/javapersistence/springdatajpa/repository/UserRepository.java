1fc288ecd623fdd4c86e90edc191b6593d5867b6
Byung-In KIM <kimbi@cliensoft.com>
2023-09-19 14:42:52 +0900

    Add jpa query method examples

--- src/main/java/com/manning/javapersistence/springdatajpa/repository/UserRepository.java
+++ src/main/java/com/manning/javapersistence/springdatajpa/repository/UserRepository.java
@@ -3,5 +3,36 @@ package com.manning.javapersistence.springdatajpa.repository;
 import com.manning.javapersistence.springdatajpa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+  User findByUsername(String username);
+
+  List<User> findAllByOrderByUsernameAsc(LocalDate start, LocalDate end);
+
+  List<User> findByUsernameAndEmail(String username, String email);
+
+  List<User> findByUsernameOrEmail(String username, String email);
+
+  List<User> findByUsernameIgnoreCase(String username);
+
+  List<User> findByLevelOrderByUsernameDesc(int level);
+
+  List<User> findByLevelGreaterThanEqual(int level);
+
+  List<User> findByUsernameContaining(String text);
+
+  List<User> findByUsernameLike(String text);
+
+  List<User> findByUsernameStartingWith(String start);
+
+  List<User> findByUsernameEndingWith(String end);
+
+  List<User> findByActive(boolean active);
+
+  List<User> findByRegistrationDateIn(Collection<LocalDate> dates);
+
+  List<User> findByRegistrationDateNotIn(Collection<LocalDate> dates);
 }
