859778738ca6bb705327d2fd88989613a344f961
Byung-In KIM <kimbi@cliensoft.com>
2023-09-19 17:20:27 +0900

    test: add test for user queries using @Query annotation

--- src/test/java/com/manning/javapersistence/springdatajpa/QueryResultsTest.java
+++ src/test/java/com/manning/javapersistence/springdatajpa/QueryResultsTest.java
@@ -2,9 +2,13 @@ package com.manning.javapersistence.springdatajpa;
 
 import com.manning.javapersistence.springdatajpa.model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 
+import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class QueryResultsTest extends SpringdatajpaScratchApplicationTests {
@@ -16,5 +20,19 @@ class QueryResultsTest extends SpringdatajpaScratchApplicationTests {
     ) {
       assertEquals(4, result.count());
     }
+  }
+
+  @Test
+  void testFindByAsArrayAndSort() {
+    List<Object[]> usersList1 =
+        userRepo.findByAsArrayAndSort("a", Sort.by("username"));
+    List<Object[]> usersList2 =
+        userRepo.findByAsArrayAndSort("a", Sort.by("email_length").descending());
+    List<Object[]> usersList3 =
+        userRepo.findByAsArrayAndSort("a", JpaSort.unsafe("LENGTH(u.email)"));
+
+    assertAll(
+        () -> assertEquals(7, usersList1.size())
+    );
   }
 }
