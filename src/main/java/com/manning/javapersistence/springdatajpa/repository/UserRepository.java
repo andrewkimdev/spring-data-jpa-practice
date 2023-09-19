d76083598abefcf1f3c48fc6d7472adaa9b78e1b
Byung-In KIM <kimbi@cliensoft.com>
2023-09-19 16:57:15 +0900

    feature: add user queries using @Query annotation

--- src/main/java/com/manning/javapersistence/springdatajpa/repository/UserRepository.java
+++ src/main/java/com/manning/javapersistence/springdatajpa/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.util.Streamable;
 
 import java.time.LocalDate;
@@ -59,4 +61,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Streamable<User> findByEmailContaining(String text);
 
   Streamable<User> findByLevel(int level);
+
+  // Using @Query annotation
+  @Query("select count(u) from User u where u.active = ?1")
+  int findNumberOfUsersByActivity(boolean active);
+
+  @Query("select u from User u where u.level = :level")
+  List<User> findByLevelAndActive(@Param("level") int level, @Param("active") boolean active);
+
+  @Query("select u.username, LENGTH(u.email) as emai_length from #{#entityName} u where u.username like %?1%")
+  List<Object[]> findByAsArrayAndSort(String text, Sort sort);
 }
