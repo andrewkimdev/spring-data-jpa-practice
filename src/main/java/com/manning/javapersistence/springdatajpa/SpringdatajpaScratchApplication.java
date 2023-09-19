e19b54127c3565afe8a031f24779ec4838efdab0
Byung-In KIM <kimbi@cliensoft.com>
2023-09-19 13:23:43 +0900

    initial commit

---
+++ src/main/java/com/manning/javapersistence/springdatajpa/SpringdatajpaScratchApplication.java
@@ -0,0 +1,13 @@
+package com.manning.javapersistence.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaScratchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringdatajpaScratchApplication.class, args);
+  }
+
+}
